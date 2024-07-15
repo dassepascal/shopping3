1f765613bb2ac85a2f4cbcb782d2154787981b14
dassepascal <pascaldasse56@gmail.com>
2024-07-15 17:23:08 +0200

    count ok

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers\Api;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\CartRepository;
 
 class CartController extends Controller
 {
    public function store(Request $request)
    {
     $product = Product::where('id', $request->productId)->first();
-
-    return $product;
+$count = (new CartRepository())->add($product);
+    return response()->json(['count' => $count]);
    }
 }
