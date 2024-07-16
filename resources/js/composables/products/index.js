f4246ada98b7f034203b1d7f6718088b5ea773d5
dassepascal <pascaldasse56@gmail.com>
2024-07-16 13:46:22 +0200

    add const getCount inside products/index.js

--- resources/js/composables/products/index.js
+++ resources/js/composables/products/index.js
@@ -3,7 +3,12 @@ export default function useProducts() {
         let response = await axios.post('/api/products',{
             productId: productId
         });
-        console.log(response)
+
+        console.log(response);
+    }
+    const getCount = async() => {
+        let response = await axios.get('/api/products/count');
+        return response.data.count
     }
 
     return {
