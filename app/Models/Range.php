214cc42fa0e29485af69f4c315fb7d8ec96158f8
dassepascal <pascaldasse56@gmail.com>
2024-07-19 16:10:34 +0200

    add colissimo

---
+++ app/Models/Range.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Range extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'max',
+    ];
+    public $timestamps = false;
+}
